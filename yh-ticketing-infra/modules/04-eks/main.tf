486d0a496390359f35788529d96f0942ce6091ac
yh <yhoka@naver.com>
2025-12-16 23:18:34 +0900

    12/16 variables must only one declaration

--- yh-ticketing-infra/modules/04-eks/main.tf
+++ yh-ticketing-infra/modules/04-eks/main.tf
@@ -1,10 +1,5 @@
 # Module 04-eks : EKS Cluster, Private Subnet에 매치, Public Subnet에는 EKS를 위한 NLB 배치
 
-# vpc/subnet 정보
-variable "vpc_id" { }
-variable "private_subnet_ids" { }
-variable "public_subnet_ids" { }
-
 # EKS 클러스터용 IAM Role
 # EKS 컨트롤 플레인이 AWS 리소스를 관리할 권한 정의
 resource "aws_iam_role" "eks_cluster_role" {
