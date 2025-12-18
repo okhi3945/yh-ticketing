7dc6bc3d949ab274f57f48a55723a03e475e9c2c
yh <yhoka@naver.com>
2025-12-18 20:45:27 +0900

    12/18 add spring boot project

---
+++ src/main/java/com/yh/yh_ticketing/YhTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.yh.yh_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YhTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yh/yh_ticketing/YhTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yh.yh_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
